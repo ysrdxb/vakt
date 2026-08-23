f0a9520f0685c3889e5e63bdfc50ad658a52ed00
Yasir <yasir@kunnatta.is>
2026-08-24 02:20:05 +0500

    fix: revert custom livewire routes to fix 500 error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,14 +22,5 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') !== 'local') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
-
-        // Custom Livewire routes to bypass server interception
-        \Livewire\Livewire::setUpdateRoute(function ($handle) {
-            return \Illuminate\Support\Facades\Route::post('/livewire-api-update', $handle);
-        });
-
-        \Livewire\Livewire::setScriptRoute(function ($handle) {
-            return \Illuminate\Support\Facades\Route::get('/livewire-js-asset', $handle);
-        });
     }
 }
